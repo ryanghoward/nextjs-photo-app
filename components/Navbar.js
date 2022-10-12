62f67fbe14cc51f4c2f5e73e3316640089aa80c8
Ryan Howard <ryanghoward@protonmail.com>
2022-10-12 14:40:22 -0700

    Nav pt 2

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#fff");
+        setTextColor("#000");
+      } else {
+        setColor("transparent");
+        setTextColor("#fff");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className='fixed left-0 top-0 w-full z-10 ease-in duration-300'
+    >
       <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
         <Link href='/'>
-          <h1 className='font-bold text-4xl'>Fotawgraphee</h1>
+          <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl'>
+            Fotawgrafee
+          </h1>
         </Link>
-        <ul className='hidden sm:flex'>
+        <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
           <li className='p-4'>
             <Link href='/'>Home</Link>
           </li>
@@ -30,8 +50,12 @@ const Navbar = () => {
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
-        <div className='block sm:hidden z-10'>
-          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        <div onClick={handleNav} className='block sm:hidden z-10'>
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
         </div>
         <div
           className={
